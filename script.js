73f9f36d4150bdc1c24b721b99d63e67ccc89b18
rafaelib <rafaeliunes97@gmail.com>
2021-04-16 15:49:16 -0300

    display do chat

---
+++ script.js
@@ -0,0 +1,65 @@
+let username="";
+
+entrarChat();
+
+function entrarChat() {
+    username = prompt("qual seu nome de usuario?");
+    const usuario = {name: username};
+    const promessa = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants", usuario);
+    promessa.then(aceitarLogin);
+    promessa.catch(negarLogin);
+}
+
+function negarLogin(erro){
+    if(erro.response.status === 400){
+        alert("Tente outro nome, pois este já está sendo usado");
+        entrarChat();
+    }
+}
+
+function aceitarLogin(){
+    atualizarMensagens();
+    setInterval(atualizarMensagens, 3000);
+    avisarServidor();
+    setInterval(avisarServidor, 5000);
+}
+
+function atualizarMensagens(){
+    const promessa = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages");
+    promessa.then(mostrarMensagens);
+}
+
+function mostrarMensagens(resposta){
+    const mensagensChat = document.querySelector(".chat");
+    mensagensChat.innerHTML = "";
+    for(let i = 0; i<resposta.data.length; i++){
+        if(resposta.data[i].type === 'status'){
+            mensagensChat.innerHTML += `
+            <li class="movimentacao-sala">
+                <span class='tempo'>(${resposta.data[i].time})</span> <span class='nome-usuarios'>${resposta.data[i].from}</span> <span>${resposta.data[i].text}</span> 
+            </li>
+                `;
+            }else if ( resposta.data[i].to === username && resposta.data[i].type === 'private_message' ){
+                mensagensChat.innerHTML += `
+                <li class="privada">
+                    <span class='tempo'>(${resposta.data[i].time})</span> <span class='nome-usuarios'>${resposta.data[i].from}</span> reservadamente para <span class='nome-usuarios'>${resposta.data[i].to}</span>: <span>${resposta.data[i].text}</span>
+                </li>
+                `;
+            }else{
+                mensagensChat.innerHTML += `
+                <li>
+                    <span class='tempo'>(${resposta.data[i].time})</span> <span class='nome-usuarios'>${resposta.data[i].from}</span> para <span class='nome-usuarios'>${resposta.data[i].to}:</span> <span>${resposta.data[i].text}</span>
+                </li>
+                `;
+            }
+    }  
+    
+}
+
+function avisarServidor(){
+    const promessa = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status', {name:username});
+    promessa.catch(recarregarPagina);
+}
+function recarregarPagina(){
+    window.location.reload();
+}
